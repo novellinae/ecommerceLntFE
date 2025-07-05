cd37b69a88ee484a28817ceed29aad3eaef114b6
Lavelinee <novellina.e@gmail.com>
2025-07-05 15:10:11 +0700

    Allow DummyJSON image domain

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['i.dummyjson.com'], // tambahkan nama domain gambar DummyJSON
+  },
+};
+
+module.exports = nextConfig;
